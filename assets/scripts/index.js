8b764cb1c8508c0b8b5beedb3b505adbc3b1fc86
tfavazza <tara.favazza@gmail.com>
2016-07-08 15:59:10 -0400

    Use Javascript to upload files

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,3 +5,38 @@
 
 // use require without a reference to ensure a file is bundled
 require('./example');
+
+$(() => {
+  $('#application-x-www-form-urlencoded').on('submit', (event) => {
+    let getFormFields = require('../../lib/get-form-fields');
+
+    event.preventDefault();
+
+    $.ajax({
+      method: 'POST',
+      url: 'http://httpbin.org/post',
+      data: getFormFields(event.target),
+
+    }).done((data)=> {
+      return console.log(data);
+    }).fail((error) => {
+      console.error(error);
+    });
+  });
+
+  $('#multipart-form-data').on('submit', (event) => {
+    event.preventDefault();
+
+    $.ajax({
+      method: 'POST',
+      url: 'http://httpbin.org/post',
+      data: new FormData(event.target),
+      contentType: false,
+      processData: false,
+    }).done((data)=> {
+      return console.log(data);
+    }).fail((error) => {
+      console.error(error);
+    });
+  });
+});
